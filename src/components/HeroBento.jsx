6097cbccb6f0c11492dc31ad24a5374c7497e664
ilariamotta <ilaria.motta93@gmail.com>
2026-02-03 15:38:06 +0100

    feat: adding imgIcon herobuttons & styling

--- src/components/HeroBento.jsx
+++ src/components/HeroBento.jsx
@@ -1,6 +1,6 @@
 import HeroCategoryButtons from "./HeroCategoryButtons";
 
-export default function BentoHero() {
+export default function BentoHero({imgIcon}) {
     return (
         <section className="grid grid-cols-1 gap-4 lg:grid-cols-12">
             {/* VIDEO*/}
@@ -15,13 +15,13 @@ export default function BentoHero() {
             {/* BOTTONI*/}
             <div className="lg:col-span-4">
                 <div className="h-full rounded-2xl border bg-white p-5 shadow-sm">
-                    <h3 className="text-lg font-semibold">Categorie</h3>
-                    <p className="mt-1 text-sm text-zinc-600">Vai dritta al punto.</p>
+                    <h3 className="text-lg font-bold text-center text-[#ff006e]">Categorie</h3>
+                    <p className="text text-zinc-600 font-semibold text-center">Inizia i tuoi acquisti!</p>
 
                     <div className="mt-4 grid gap-3">
-                        <HeroCategoryButtons to="/categories/videogames" label="Videogiochi" />
-                        <HeroCategoryButtons to="/categories/consoles" label="Console" />
-                        <HeroCategoryButtons to="/categories/accessories" label="Accessori" />
+                        <HeroCategoryButtons to="/categories/videogames" label="VIDEOGIOCHI"  imgIcon="/images/icons/videogames.png"/>
+                        <HeroCategoryButtons to="/categories/consoles" label="CONSOLE" imgIcon="/images/icons/consoles.png" />
+                        <HeroCategoryButtons to="/categories/accessories" label="ACCESSORI" imgIcon="/images/icons/accessories.png" />
                     </div>
                 </div>
             </div>
--- src/components/HeroCategoryButtons.jsx
+++ src/components/HeroCategoryButtons.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom";
 
-export default function HeroCategoryButtons({ to, label }) {
+export default function HeroCategoryButtons({ to, label, imgIcon }) {
+
+
   return (
-    <NavLink
-      to={to}
-      className="flex h-20 items-center justify-center rounded-2xl border border-white/10 bg-zinc-900/85 px-4 text-sm font-semibold text-white backdrop-blur transition hover:bg-zinc-900 hover:scale-[1.01] active:scale-[0.99]"
-    >
-      {label}
+    <NavLink to={to} className="relative flex h-20 items-center rounded-2xl border border-white/10 bg-zinc-900/85 px-4 text-sm font-semibold text-white backdrop-blur transition hover:bg-zinc-900 hover:scale-[1.01] active:scale-[0.99]">
+        <img src={imgIcon} alt="" className="h-10 w-10 flex-shrink-0 object-contain"/>
+      <span className="absolute left-1/2 -translate-x-1/2 text-lg sm:text-xl font-bold tracking-wide drop-shadow-[0_0_8px_rgba(255,210,31,0.75)]">{label}</span>
     </NavLink>
   );
 }
