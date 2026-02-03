e3aa3219eef1b3b0bfa5e286c9a9be0c8438495d
ilariamotta <ilaria.motta93@gmail.com>
2026-02-03 17:01:19 +0100

    feat: adding button & product card components, adding grid in homepage

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,46 @@
+import Button from "./Button";
+
+export default function ProductCard({ product = {} }) {
+    const {
+        name = "Prodotto",
+        description = "",
+        price = 0,
+        image,
+    } = product;
+
+    const imgSrc =
+        image || "https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg";
+
+    return (
+        <div className="group relative overflow-hidden rounded-2xl border border-white/10 bg-[#211a1d] shadow-sm transition hover:shadow-lg hover:scale-[1.01]">
+            {/* IMMAGINE */}
+            <div className="relative">
+                <img src={imgSrc} alt={name} className="h-65 w-full object-cover transition duration-300 group-hover:scale-105" />
+                {/* ANIMAZIONE OVERLAY */}
+                <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-black/40 via-black/0 to-black/0" />
+            </div>
+
+            {/* CONTENTO */}
+            <div className="p-4">
+                {/* titolo prodotto */}
+                <h3 className="mb-1 line-clamp-2 text-base font-semibold text-[#ff006e]">
+                    {name}</h3>
+
+                {/* descrizione */}
+                    <p className="mb-3 line-clamp-2 text-sm text-zinc-300">
+                        {description}
+                    </p>
+
+                {/* prezzo */}
+                <div className="flex items-center justify-between">
+                    <p className="text-lg font-bold text-[#ffd21f]">
+                        €{price?.toFixed(2)}
+                    </p>
+
+                    <Button text="Dettagli"
+                        className="relative rounded-xl border border-[#00BFFF]/70 px-4 py-2 text-xs font-semibold text-[#00BFFF] bg-transparent transition-all duration-300 ease-out hover:border-[#00BFFF] hover:bg-[#00BFFF]/10 hover:shadow-[0_0_16px_rgba(0,191,255,0.45)] hover:scale-[1.04] active:scale-[0.97]" />
+                </div>
+            </div>
+        </div>
+    );
+}
