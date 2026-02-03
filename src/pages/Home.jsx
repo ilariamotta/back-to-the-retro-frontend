426bd0f5b1cd01dbe1ac0e43116da79169d98d0e
ilariamotta <ilaria.motta93@gmail.com>
2026-02-03 12:29:04 +0100

    feat: container home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
-    return (
-        <>
-        <h1 className="py-5">Homepage</h1>
-        <p>prova</p>
-        </>
-    )
+  return (
+    <div className="space-y-10">
+      {/* CONTAINER */}
+      <div className="mx-auto max-w-7xl px-5 sm:px-6 lg:px-8 py-8">
+        <h1 className="text-4xl font-bold text-black">Home Page</h1>
+      </div>
+    </div>
+  );
 }
