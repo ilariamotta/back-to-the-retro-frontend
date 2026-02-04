c427bf98c84f6c2d9641edaee07fc2a8725fe50b
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-02-04 12:15:47 +0100

    feat: backend connesso al front, e stampati in pagina tutti i prodotti wip: filtro per categorie e dettagli

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
 // import ClientDataForm from "../components/ClientDataForm";
+import axios from "axios";
 import HeroBento from "../components/HeroBento";
 import ProductCard from "../components/ProductCard";
+import { useState, useEffect } from "react";
 
-const products = [
-    { id: 1, name: "Sample Game A", price: 49.99, image: "https://via.placeholder.com/600x400.png?text=Game+A", description: "Descrizione breve A" },
-    { id: 2, name: "Sample Console B", price: 299.99, image: "https://via.placeholder.com/600x400.png?text=Console+B", description: "Descrizione breve B" },
-    { id: 3, name: "Accessory C", price: 19.99, image: "https://via.placeholder.com/600x400.png?text=Accessory+C", description: "Descrizione breve C" },
-    { id: 3, name: "Accessory C", price: 19.99, image: "https://via.placeholder.com/600x400.png?text=Accessory+C", description: "Descrizione breve C" },
-];
+
 
 export default function Home() {
+
+    const addressIndex = import.meta.env.VITE_BACKEND_URL;
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/retro/api/products").then((resp) => {
+            const risposta = resp.data.results
+            setProducts(risposta)
+        })
+    }, [])
+
+    
+
     return (
         <div className="space-y-10">
             {/* CONTAINER */}
@@ -20,7 +30,13 @@ export default function Home() {
                 <h1 className="text-3xl font-bold text-start text-[#ff006e] mt-8 mb-6 drop-shadow-[0_0_8px_rgba(255,0,110,0.75)]">I NOSTRI PRODOTTI</h1>
                 {/* GRIGLIA PRODOTTI */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-                    {products.map((p) => <ProductCard key={p.id} product={p} />)}
+                  {products.map((p)=>{
+                    return (
+                        <>
+                            <ProductCard product={p}/>
+                        </>
+                    )
+                  })}
                 </div>
             </div>
         </div>
