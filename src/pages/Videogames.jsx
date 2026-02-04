57d3a22e085c666b3878c3aa5e6c9701cc4e5be1
murphy-afk <bardarenoemi@gmail.com>
2026-02-04 18:46:50 +0100

    feat: product undefined case

--- src/pages/Videogames.jsx
+++ src/pages/Videogames.jsx
@@ -3,14 +3,13 @@ import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 
 export default function Videogames() {
-     const addressIndex = import.meta.env.VITE_BACKEND_URL;
+    const addressIndex = import.meta.env.VITE_BACKEND_URL;
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         axios.get(`${addressIndex}retro/api/products?categories=Videogiochi`).then((resp) => {
-            const risposta = resp.data.results;
+            const risposta = resp.data.results || resp.data.result || [];
             setProducts(risposta);
-            console.log(resp.data.results);
         });
     }, []);
     return (
@@ -26,13 +25,15 @@ export default function Videogames() {
                 {/* TITOLO */}
                 <h1 className="text-3xl font-bold text-start text-[#ff006e] mt-8 mb-6 drop-shadow-[0_0_8px_rgba(255,0,110,0.75)]">VIDEOGIOCHI</h1>
                 {/* GRIGLIA PRODOTTI */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
+                { products !== undefined ? <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
                     {products.map((p) => {
                         return (
                             <ProductCard product={p} key={p.id} />
                         )
                     })}
-                </div>
+                </div> :
+                <p>No products available</p>
+                }
             </div>
         </div>
     );
