e61bfcdc5ab6d16aae72a46321f91b72914502fc
murphy-afk <bardarenoemi@gmail.com>
2026-02-04 17:53:49 +0100

    feat: consoles page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
     const addressIndex = import.meta.env.VITE_BACKEND_URL;
     const [products, setProducts] = useState([]);
-    
+
     useEffect(() => {
         axios.get(`${addressIndex}retro/api/products`).then((resp) => {
             const risposta = resp.data.results
@@ -18,7 +18,7 @@ export default function Home() {
         })
     }, [])
 
-    
+
 
     return (
         <div className="space-y-10">
@@ -30,11 +30,11 @@ export default function Home() {
                 <h1 className="text-3xl font-bold text-start text-[#ff006e] mt-8 mb-6 drop-shadow-[0_0_8px_rgba(255,0,110,0.75)]">I NOSTRI PRODOTTI</h1>
                 {/* GRIGLIA PRODOTTI */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-                  {products.map((p)=>{
-                    return (
-                            <ProductCard product={p} key={p.id}/>
-                    )
-                  })}
+                    {products.map((p) => {
+                        return (
+                            <ProductCard product={p} key={p.id} />
+                        )
+                    })}
                 </div>
             </div>
         </div>
