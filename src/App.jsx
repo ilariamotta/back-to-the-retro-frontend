4e309dc69c9f45125425fc2288a265e5417f7154
murphy-afk <bardarenoemi@gmail.com>
2026-02-05 16:49:22 +0100

    feat: cart context

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,29 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
 import './index.css'
 import Home from './pages/Home'
-import ProductDetail from './pages/ProductDetail' 
+import ProductDetail from './pages/ProductDetail'
 import Videogames from './pages/Videogames'
 import Consoles from './pages/Console'
 import Accessories from './pages/Accessories'
+import { CartProvider } from './context/CartContext'
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route element={<Home />} path="/" />
-            <Route element={<ProductDetail />} path="/products/:slug"/>
-            <Route element={<Videogames />} path="/categories/videogames"/>
-            <Route element={<Consoles />} path="/categories/consoles"/>
-            <Route element={<Accessories />} path="/categories/accessories"/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route element={<Home />} path="/" />
+              <Route element={<ProductDetail />} path="/products/:slug" />
+              <Route element={<Videogames />} path="/categories/videogames" />
+              <Route element={<Consoles />} path="/categories/consoles" />
+              <Route element={<Accessories />} path="/categories/accessories" />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </>
   )
 }
