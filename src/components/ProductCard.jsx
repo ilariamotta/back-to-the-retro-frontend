fe30f131d711d2572f7f68bcc9dbdae9cb63ece4
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-02-06 20:20:06 +0100

    Feat: bottone aggiungi al carrello, bottone aggiungi alla wishlist, WISHLISTCONTEXT preparato WIP: pagina wishlist

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { getProductImageUrl } from "../utils/imageUtils";
+import { useCart } from "../context/CartContext";
 
 export default function ProductCard({ product }) {
 
+    const { addToCart } = useCart()
     const navigate = useNavigate()
     function details() {
         navigate(`/products/${product.slug}`)
@@ -39,22 +41,45 @@ export default function ProductCard({ product }) {
                 </p>
 
                 {/* prezzo */}
-                <div className="flex items-center justify-between ">
-                   
+                <div className="flex items-center justify-between">
+
                     <p>
-                            {/* prezzo originale  */}
-                            <span className={product.discounted_price !== null ? "text-lg font-bold text-[#ffe417] line-through" : "hidden"}>€ {product.price}</span><br />
+                        {/* prezzo originale  */}
+                        <span className={product.discounted_price !== null ? "text-lg font-bold text-[#ffe417] line-through" : "hidden"}>€ {product.price}<br /></span>
 
-                            <span className={product.discounted_price !== null ? "text-lg font-bold text-[#fe0000]" : "text-lg font-bold text-[#ffe417]"}>€ {product.price - product.discounted_price}</span>
-                    </p>    
-
-                    <button
-                        onClick={details}
-                        className="relative rounded-xl border border-[#00BFFF]/70 px-4 py-2 text-xs font-semibold text-[#00BFFF] bg-transparent transition-all duration-300 ease-out hover:border-[#00BFFF] hover:bg-[#00BFFF]/10 hover:shadow-[0_0_16px_rgba(0,191,255,0.45)] hover:scale-[1.04] active:scale-[0.97]">
-                        DETTAGLI
-                    </button>
+                        {/* prezzo scontato */}
+                        <span className={product.discounted_price !== null ? "text-lg font-bold text-[#fe0000]" : "text-lg font-bold text-[#ffe417]"}>€ {product.price - product.discounted_price}</span>
+                    </p>
+                    
+                    {/* BOTTONI PER IL CARELLO E DETTAGLI */}
+                    <div className="flex flex-col">
+                        <button
+                            onClick={()=>addToCart({
+                                id: product.id,
+                                slug: product.slug,
+                                name: product.name,
+                                price: product.discounted_price !== null ? product.price - product.discounted_price : product.price,
+                                stock: product.stock,
+                                cover_image: product.cover_image,
+                            })}
+                            className="relative rounded-xl border border-[#00BFFF]/70 px-4 py-2 text-xs font-semibold text-[#00BFFF] bg-transparent transition-all duration-300 ease-out hover:border-[#00BFFF] hover:bg-[#00BFFF]/10 hover:shadow-[0_0_16px_rgba(0,191,255,0.45)] hover:scale-[1.04] active:scale-[0.97] mb-2">
+                            CARRELLO
+                        </button>
+                        <button
+                            onClick={details}
+                            className="relative rounded-xl border border-[#00BFFF]/70 px-4 py-2 text-xs font-semibold text-[#00BFFF] bg-transparent transition-all duration-300 ease-out hover:border-[#00BFFF] hover:bg-[#00BFFF]/10 hover:shadow-[0_0_16px_rgba(0,191,255,0.45)] hover:scale-[1.04] active:scale-[0.97]">
+                            DETTAGLI
+                        </button>
+                    </div>
+                    {/* PULSANTE PER WHISHLIST */}
+                    <button 
+                    
+                    className="absolute top-1 end-1 rounded-xl border border-[#fe0000]/70 px-4 py-2 text-xs font-semibold text-[#ff0000] bg-transparent transition-all duration-300 ease-out hover:border-[#d0ff00] hover:bg-[#fe0000]/10 hover:shadow-[0_0_16px_rgba(0,191,255,0.45)] hover:scale-[1.04] active:scale-[0.97]">like</button>
                 </div>
+
+
+
             </div>
-        </div>
+        </div >
     );
 }
